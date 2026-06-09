dfe9743bbe9a1c8b5470903f43025661a9cf983d
DaViT <43728693+datoi@users.noreply.github.com>
2026-06-09 12:00:51 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+jsimport react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
@@ -16,7 +16,14 @@ export default defineConfig({
         jsx: 'automatic',
     },
     server: {
-        host: 'localhost',
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
         cors: true,
+        hmr: process.env.CODESPACE_NAME ? {
+            host: `${process.env.CODESPACE_NAME}-5173.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+            protocol: 'wss',
+            clientPort: 443,
+        } : undefined,
     },
 });
